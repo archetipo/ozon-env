e72e724fde5c0e713176761b6b495b486b1512ed
Alessio Gerace <alessio.gerace@gmail.com>
2023-08-08 11:19:45 +0200

    [FIX] tests script

--- run_test.sh
+++ run_test.sh
@@ -2,13 +2,13 @@
 echo "update system"
 poetry update
 echo "make compose"
-docker-compose  up -d --force-recreate
+docker compose  up -d --force-recreate
 echo "check code"
 poetry run black ozonenv/**/*.py
 poetry run flake8 ozonenv/**/*.py
 pip install --upgrade -e .
 echo "run test"
 poetry run pytest --cov --cov-report=html -vv
-docker-compose down
+docker compose down
 rm -rf tests/models
 echo "make project: Done."

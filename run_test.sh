047002ee12ef65e71a16b043b3d7208565abb2ca
Lori842 <lori16078@gmail.com>
2023-10-19 16:58:08 +0200

    flake8 removed

--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,7 @@ echo "make compose"
 docker compose  up -d --force-recreate
 echo "check code"
 poetry run black ozonenv/**/*.py
-poetry run flake8 ozonenv/**/*.py
+#poetry run flake8 ozonenv/**/*.py
 pip install --upgrade -e .
 echo "run test"
 poetry run pytest --cov --cov-report=html -vv
